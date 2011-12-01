00d2763eba7b5fe7423a35d5a5f549f2779d7618
kfihihc <kfihihc@gmail.com>
2011-12-01 10:43:31 +0800

    change the UI layout

--- gen/com/seeedstudio/btkit/R.java
+++ gen/com/seeedstudio/btkit/R.java
@@ -16,14 +16,15 @@ public final class R {
     public static final class id {
         public static final int button_scan=0x7f050006;
         public static final int button_send=0x7f05000a;
-        public static final int connect_button=0x7f05000e;
+        public static final int connect_button=0x7f05000f;
         public static final int edit_text_out=0x7f050009;
         public static final int in=0x7f050007;
-        public static final int more_action=0x7f05000c;
-        public static final int more_root=0x7f05000d;
+        public static final int led_toggle_button=0x7f05000c;
+        public static final int more_action=0x7f05000d;
+        public static final int more_root=0x7f05000e;
         public static final int new_devices=0x7f050005;
         public static final int paired_devices=0x7f050003;
-        public static final int test_button=0x7f05000f;
+        public static final int test_button=0x7f050010;
         public static final int title_left_text=0x7f050000;
         public static final int title_new_devices=0x7f050004;
         public static final int title_paired_devices=0x7f050002;
@@ -41,38 +42,39 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
-        public static final int battery=0x7f04001a;
+        public static final int battery=0x7f04001b;
         public static final int bt_not_enabled_leaving=0x7f040004;
-        public static final int button_scan=0x7f040018;
-        public static final int cancel=0x7f04000e;
+        public static final int button_scan=0x7f040019;
+        public static final int cancel=0x7f04000f;
         /**  BTkitChart 
          */
-        public static final int chart=0x7f040019;
+        public static final int chart=0x7f04001a;
         public static final int close=0x7f04000c;
         public static final int connect=0x7f040008;
         public static final int discoverable=0x7f040009;
-        public static final int get_and_chart=0x7f040011;
+        public static final int get_and_chart=0x7f040012;
         public static final int hello=0x7f040000;
-        public static final int line_chart=0x7f04001b;
-        public static final int more=0x7f04000f;
-        public static final int more_action=0x7f040010;
-        public static final int none_found=0x7f040015;
-        public static final int none_paired=0x7f040014;
+        public static final int led_open=0x7f04000d;
+        public static final int line_chart=0x7f04001c;
+        public static final int more=0x7f040010;
+        public static final int more_action=0x7f040011;
+        public static final int none_found=0x7f040016;
+        public static final int none_paired=0x7f040015;
         public static final int not_available=0x7f04000a;
         public static final int not_connected=0x7f040003;
         public static final int open=0x7f04000b;
         /**  BTkitDeviceList 
          */
-        public static final int scanning=0x7f040012;
-        public static final int select_device=0x7f040013;
+        public static final int scanning=0x7f040013;
+        public static final int select_device=0x7f040014;
         /**  BTkitMain 
          */
         public static final int send=0x7f040002;
         public static final int title_connected_to=0x7f040006;
         public static final int title_connecting=0x7f040005;
         public static final int title_not_connected=0x7f040007;
-        public static final int title_other_devices=0x7f040017;
-        public static final int title_paired_devices=0x7f040016;
-        public static final int value=0x7f04000d;
+        public static final int title_other_devices=0x7f040018;
+        public static final int title_paired_devices=0x7f040017;
+        public static final int value=0x7f04000e;
     }
 }
